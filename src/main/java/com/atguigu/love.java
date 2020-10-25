9e125db21978f402c7327fbd7760ccec9474429a
TiangeGuo <2027795568@qq.com>
2020-10-26 00:12:51 +0800

    增加了几行代码

--- src/main/java/com/atguigu/love.java
+++ src/main/java/com/atguigu/love.java
@@ -4,5 +4,8 @@ public class love {
     public static void main(String[] args) {
         System.out.println("1111111111111"+2222);
         System.out.println("1111111111111"+3333);
+        System.out.println("1111111111111"+3333);
+        System.out.println("1111111111111"+3333);
+        System.out.println("1111111111111"+3333);
     }
 }
